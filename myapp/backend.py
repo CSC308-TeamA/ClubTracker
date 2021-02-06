bfba636ab109a06366f3e08cf1ae17e4bfedb897
Ro-Bert-Web <ro-bertbhs424@gmail.com>
2021-02-06 10:59:03 -0800

    Setup Backend page ports

--- myapp/backend.py
+++ myapp/backend.py
@@ -3,5 +3,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World!';
 
+@app.route("/about")
+def AboutPage():
+   return "Don't know, but About Page!!!";
+
+@app.route("/contact")
+def ContactPage():
+   return "We'll probably put something here like \"links to contact\" and \"email API\"";
