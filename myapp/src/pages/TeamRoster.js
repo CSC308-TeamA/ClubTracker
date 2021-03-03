04aedbef9f251b00dd0a7780e13742fc1ed9d806
Ro-Bert-Web <ro-bertbhs424@gmail.com>
2021-03-03 00:18:10 -0800

    Added team roster filtering to front end

--- myapp/src/pages/TeamRoster.js
+++ myapp/src/pages/TeamRoster.js
@@ -3,13 +3,26 @@ import axios from 'axios'
 import Card from 'react-bootstrap/Card'
 import websiteBanner from '../assets/websiteBanner.jpg'
 import profileSilhouette from '../assets/profileSilhouette.jpg'
+import Form from './FilterForm.js'
 
 function TeamRoster() {
   const [characters, setCharacters] = useState([]);
 
-  async function fetchAll() {
+  async function fetch(field) {
     try {
-      const response = await axios.get('http://localhost:5000/teamroster');
+      var link = "http://localhost:5000/teamroster?"
+      if (field.name !== "")
+        link += "name=" + field.name + "&";
+      if (field.stat !== "")
+        link += "status=" + field.stat + "&";
+      if (field.role !== "")
+        link += "role=" + field.role + "&";
+      if (field.posit !== "")
+        link += "position=" + field.posit + "&";
+      if (field.special !== "")
+        link += "specialization=" + field.special + "&";
+
+      const response = await axios.get(link);
       return response.data;
     }
 
@@ -20,11 +33,25 @@ function TeamRoster() {
   }
 
   useEffect(() => {
-    fetchAll().then(result => {
+    fetch({
+      name: '',
+      stat: '',
+      role: '',
+      posit: '',
+      special: ''
+    }).then(result => {
       if (result)
         setCharacters(result);
     });
   }, []);
+
+  function updatePage(field) {
+    console.log(field);
+    fetch(field).then(result => {
+      if (result)
+        setCharacters(result);
+    });
+  }
 
   const list = characters.map((row, index) => {
     return (
@@ -47,6 +74,9 @@ function TeamRoster() {
   <div>
     <div style={{ paddingTop: '15px', paddingBottom: '20px', display: 'flex', justifyContent: 'center'}}>
       <img src={ websiteBanner } />
+    </div>
+    <div>
+      <Form handleSubmit = {updatePage} />
     </div>
     <div style={{ display: 'flex', flexWrap: 'wrap' }}>
       {list}
