5e2ea1d845cd9831e33def11a9ee8d1dc0f83369
Jerbilcraft <jburiane@calpoly.edu>
2021-03-03 10:48:20 -0800

    added first method for buildAndRun.sh plus support

---
+++ Scripts/buildAndRun.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+#
+read -p 'This program requires python 3 (python), pip and npm installed. Press [Enter] to begin...'
+#check if npm is installed
+npm --version || read -p 'npm is not installed. Press [Enter] to terminate.' || exit
+#check if python is installed
+python --version || read -p 'Python is not installed. Press [Enter] to terminate.' || exit
+
+cd ../myapp
+
+echo 'bootstraping npm'
+npm install react-bootstrap bootstrap
+
+cd backend
+
+echo 'installing flask'
+pip install flask
+echo 'installing flask-cors'
+pip install flask-cors
+echo 'installing axios'
+npm install axios
+
+export FLASK_APP=backend.py
+export FLASK_ENV=development
+echo 'STARTING BACKEND'
+exec ../../Scripts/startBackend.sh &
+
+cd ..
+
+echo 'STARTING FRONTEND'
+exec ../Scripts/startFrontend.sh &
+
+wait
+done
