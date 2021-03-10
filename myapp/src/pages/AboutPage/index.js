a4845c8d669cfe7608d06c618a2dd07af2e612b6
Jerbilcraft <jburiane@calpoly.edu>
2021-03-10 11:19:11 -0800

    added about page info

--- myapp/src/pages/AboutPage/index.js
+++ myapp/src/pages/AboutPage/index.js
@@ -15,29 +15,29 @@ export const About = () => (
         <p>Welocome to Robotics (####) team page.</p>
         <p>We are part of FIRST Robotics and our rookie year was ####</p>
         <p>Please explore our webpage and social media</p>
+        <p>Just as the on-going space program does this, Bear Metal continues to instill interest in STEM and business careers while creating opportunities for students to broaden their educational horizons and simultaneously develop their interpersonal skills. We prepare members for the workforce by emphasizing hands on opportunities, accountability, teamwork, and leadership, which are the core values we impress upon our members. For us, robotics is not only about the skills we learn, but the lasting experiences that are the foundation of lifelong relationships.</p>
+
+        <h2>COMMUNITY</h2>
+        <p>Bear Metal focuses on fostering the curiosity of children. Within the Maple Valley community there are seven FLL teams, all started by Bear Metal alumni and students who have attended past robotics camps. Four of the FLL teams are supported through our school district by Bear Metal mentors and members. FLL has also inspired the creation of an after-school robotics club at Maple View Middle School for students who cannot make the full-time commitment that FLL requires, but are still interested in robotics.</p>
         
         <h2>OUR MISSION</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        <p>Bear Metal strives to offer members and mentors the opportunities to develop their own skills, reach new people, and make an impact. To achieve this, our team focuses on hands-on learning, mentoring, and teamwork. Our varied backgrounds foster personal growth. We aim to leave a positive, lasting impression in our community by inspiring more people to pursue their passions.</p>
         
         <h2>OUR HISTORY</h2>
-        <p>Welocome to Robotics (####) team page.</p>
-        <p>We are part of FIRST Robotics and our rookie year was ####</p>
-        <p>Please explore our webpage and social media</p>
+        <p>2007: Epic</p>
+        <p>2008: Catalyst</p>
+        <p>2009: Bearenstein</p>
+        <p>2010: Odyssey</p>
+        <p>2011: Ursa Major &amp; Ursa Minor</p>
+        <p>2012: Bearmageddon</p>
         
-        <h2>MENTORS AND VOLUNTEERS</h2>
-        <p>Welocome to Robotics (####) team page.</p>
-        <p>We are part of FIRST Robotics and our rookie year was ####</p>
-        <p>Please explore our webpage and social media</p>
+        <h2>DIVERSITY</h2>
+        <p>“Genius has no race. Strength has no gender”. Women like Katherine Johnson and Sally Ride propelled advancement in the space industry through their leadership and skill. Just as these women opened opportunities for females, Bear Metal has created an inclusive environment encouraging female involvement by hosting the first offseason FRC event in Washington state exclusively for females in FIRST, called Girls’ Generation. 
+</p>
         
         <h2>SUBTEAMS</h2>
-        <p>Welocome to Robotics (####) team page.</p>
-        <p>We are part of FIRST Robotics and our rookie year was ####</p>
-        <p>Please explore our webpage and social media</p>
-        
-        <h2>AWARDS</h2>
-        <p>Welocome to Robotics (####) team page.</p>
-        <p>We are part of FIRST Robotics and our rookie year was ####</p>
-        <p>Please explore our webpage and social media</p>
+        <p>At Bear Metal, we believe that the best way to gain experience is through hands on learning. Members study the six pillars of robotics: design, electronics, fabrication, assembly, programming, and business, to gain industry applicable skills. Bear Metal fosters interest in STEM learning through robotics camps, classes and workshops
+</p>
   </>
 
 )
