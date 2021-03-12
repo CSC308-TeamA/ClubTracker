164409f9d59c0e9dd879d4b109e410c9d52229da
Jerbilcraft <jburiane@calpoly.edu>
2021-03-11 17:10:14 -0800

    fist instance of database files

---
+++ myapp/mongodb_database/database.py
@@ -0,0 +1,95 @@
+import pymongo, re
+from bson import ObjectId
+
+class Database(dict):
+
+    __getattr__ = dict.get
+    __delattr__ = dict.__delitem__
+    __setattr__ = dict.__setitem__
+
+    def save(self):
+        if not self._id:
+            self.collection.insert(self)
+        else:
+            self.collection.update(
+                { "_id": ObjectId(self._id) }, self)
+        self._id = str(self._id)
+
+    def reload(self):
+        if self._id:
+            result = self.collection.find_one({"_id": ObjectId(self._id)})
+            if result :
+                self.update(result)
+                self._id = str(self._id)
+                return True
+        return False
+
+    def remove(self):
+        if self._id:
+            resp = self.collection.delete_one({"_id": ObjectId(self._id)})
+            return resp.deleted_count
+
+class TeamRoster(Database):
+    users = list(self.collection.find())
+        for user in users:
+            user["_id"] = str(user["_id"]) #converting ObjectId to str
+        return users
+
+    def find_all(self):
+        users = list(self.collection.find())
+        for user in users:
+            user["_id"] = str(user["_id"]) #converting ObjectId to str
+        return users
+
+        #db.products.find( { name: { $regex: /^name/i } } )
+        #regx = ({'files':{'$regex':'^File'}})
+        #name, status, role, specialization
+    def find_by_name(self, name):
+        regx = re.compile("^"+name, re.IGNORECASE)
+        users = list(self.collection.find({ name: regx }))
+        for user in users:
+            user["_id"] = str(user["_id"])
+        return users
+
+    def find_by_status(self, status):
+        regx = re.compile("^"+status, re.IGNORECASE)
+        users = list(self.collection.find({ name: regx }))
+        for user in users:
+            user["_id"] = str(user["_id"])
+        return users  
+
+    def find_by_role(self, role):
+        regx = re.compile("^"+role, re.IGNORECASE)
+        users = list(self.collection.find({ name: regx }))
+        for user in users:
+            user["_id"] = str(user["_id"])
+        return users  
+
+    def find_by_position(self, position):
+        regx = re.compile("^"+position, re.IGNORECASE)
+        users = list(self.collection.find({ name: regx }))
+        for user in users:
+            user["_id"] = str(user["_id"])
+        return users  
+
+
+    def find_by_specialization(self, specialization):
+        regx = re.compile("^"+specialization, re.IGNORECASE)
+        users = list(self.collection.find({ name: regx }))
+        for user in users:
+            user["_id"] = str(user["_id"])
+        return users  
+
+    def find_by_name_status(self, name, status):
+        regx = re.compile("^"+name, re.IGNORECASE)
+        users = list(self.collection.find({ name: regx }))
+        for user in users:
+            user["_id"] = str(user["_id"])
+        return users
+
+    def find_by_name_status(self, name, status):
+        regx = re.compile("^"+name, re.IGNORECASE)
+        users = list(self.collection.find({ name: regx }))
+        for user in users:
+            user["_id"] = str(user["_id"])
+        return users
