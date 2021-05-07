7cce549be4b951a8421410816439cef2ebcbbb33
Steven Ngo <stengo46@gmail.com>
2021-05-06 17:17:08 -0700

    Update localhost uses in TeamRosterPage to heroku backend link

--- myapp/src/pages/TeamRosterPage/index.js
+++ myapp/src/pages/TeamRosterPage/index.js
@@ -46,7 +46,7 @@ function TeamRoster() {
 
   async function makePostCall(person) {
     try {
-      const response = await axios.post('http://localhost:5000/teamroster', person);
+      const response = await axios.post('https://clubtracker-backend.herokuapp.com/teamroster', person);
       return response;
     } catch (error) {
       return false;
@@ -63,7 +63,7 @@ function TeamRoster() {
 
   async function makeDeleteCall(id) {
     try {
-      const response = await axios.delete(`http://localhost:5000/teamroster?_id=${id}`);
+      const response = await axios.delete(`https://clubtracker-backend.herokuapp.com/teamroster?_id=${id}`);
       return response;
     } catch (error) {
       return false;
