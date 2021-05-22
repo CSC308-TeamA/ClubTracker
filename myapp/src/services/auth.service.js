8983c551b8fe1d7093931463cdabe070fa977fc1
Jenisa Nguyen <jenisa@gmail.com>
2021-05-22 03:58:42 -0400

    localhost change

--- myapp/src/services/auth.service.js
+++ myapp/src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/auth/';
+const API_URL = 'http://localhost:8080/api/auth/';
 
 const register = (username, email, password) => axios.post(`${API_URL}signup`, {
   username,
